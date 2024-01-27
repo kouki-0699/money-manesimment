29364a1b66621181704962cbd707f9cc3cac618a
kouki331-pro <kouki.0699g@gmail.com>
2024-01-27 17:33:56 +0900

    gspread_api 実装

--- Flask/form.py
+++ Flask/form.py
@@ -1,18 +1,30 @@
 from flask import Flask, request, render_template
-from openpyxl import Workbook, load_workbook  # load_workbook を追加
 
 app = Flask(__name__)
-
-# エクセルファイル名
-excel_file = 'allowance.xlsx'
 
 @app.route('/')
 def index():
     return render_template('maney.html')
 
+#gsspread_api_infomation
 import gspread  
 from oauth2client.service_account import ServiceAccountCredentials
+sp_credential_file ='C:\Users\kouki.KOUKI\money-manesimment\Flask\money-manesimment api_infomation.json'
+sp_scope =["https://spreadsheets.google.com/feeds", 
+           "https://www.googleapis.com/auth/spreadsheets",
+           "https://www.googleapis.com/auth/drive.file", 
+           "https://www.googleapis.com/auth/drive"]
+sp_sheet_id ="1vwloBGidR-Mr8-Y8YiQfa1Y_ycMFfS89yi9kFLU2WlQ"
+sp_sheet_name =["January",]  #後で変更（シートネーム　月ごと（英語））
 
+credentials = ServiceAccountCredentials.from_json_keyfile_name(sp_credential_file, sp_scope)
+gc = gspread.authorize(credentials)
+
+sh = gc.open_by_key(sp_sheet_id)
+worksheet_January= sh.worksheet(sp_sheet_name)
+
+
+#処理
 @app.route('/process_data', methods=['POST'])
 def process_data():
     month = request.form['month']
@@ -21,30 +33,8 @@ def process_data():
     expenses = request.form['expenses']
     free_text = request.form['free_text']
     
-    # データをエクセルファイルに書き込む
-    write_to_excel(date, month, income, expenses ,free_text )
-    
+    worksheet_January.append_row([month,date,])#列に表示させる内容
     return render_template('index.html')
-
-def write_to_excel(date, item, amount):
-    wb = None
-    try:
-        # エクセルファイルを開くか新規作成
-        try:
-            wb = load_workbook(excel_file)  # 既存のワークブックを読み込む
-            ws = wb.active
-        except:
-            wb = Workbook()
-            ws = wb.active
-
-        # データを追加
-        ws.append([date, item, amount])
-
-        # ファイル保存
-        wb.save(excel_file)
-    finally:
-        if wb:
-            wb.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
