48a71035439f99a150f738914a2d21069fe454e3
kouki331-pro <kouki.0699g@gmail.com>
2024-02-01 13:08:49 +0900

    変更

--- Flask/form.py
+++ Flask/form.py
@@ -1,21 +1,36 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('maney.html')
-
 #gsspread_api_infomation
 import gspread  
 from oauth2client.service_account import ServiceAccountCredentials
-sp_credential_file ='C:\Users\kouki.KOUKI\money-manesimment\Flask\money-manesimment api_infomation.json'
+sp_credential_file ="Flask\money-manesimment api_infomation.json"
 sp_scope =["https://spreadsheets.google.com/feeds", 
            "https://www.googleapis.com/auth/spreadsheets",
            "https://www.googleapis.com/auth/drive.file", 
            "https://www.googleapis.com/auth/drive"]
 sp_sheet_id ="1vwloBGidR-Mr8-Y8YiQfa1Y_ycMFfS89yi9kFLU2WlQ"
-sp_sheet_name =["January",]  #後で変更（シートネーム　月ごと（英語））
+sp_sheet_name ="January" #後で変更（シートネーム　月ごと（英語））
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(sp_credential_file, sp_scope)
+gc = gspread.authorize(credentials)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    worksheet_January.append_row("こんにちは")
+    return render_template('maney.html')
+
+#gsspread_api_infomation
+import gspread  
+from oauth2client.service_account import ServiceAccountCredentials
+sp_credential_file ="Flask\money-manesimment api_infomation.json"
+sp_scope =["https://spreadsheets.google.com/feeds", 
+           "https://www.googleapis.com/auth/spreadsheets",
+           "https://www.googleapis.com/auth/drive.file", 
+           "https://www.googleapis.com/auth/drive"]
+sp_sheet_id ="1vwloBGidR-Mr8-Y8YiQfa1Y_ycMFfS89yi9kFLU2WlQ"
+sp_sheet_name ="January" #後で変更（シートネーム　月ごと（英語））
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name(sp_credential_file, sp_scope)
 gc = gspread.authorize(credentials)
@@ -27,6 +42,7 @@ worksheet_January= sh.worksheet(sp_sheet_name)
 #処理
 @app.route('/process_data', methods=['POST'])
 def process_data():
+    worksheet_January.append_row("こんにちは")
     month = request.form['month']
     date =request.form['date']  
     income = request.form['income']
